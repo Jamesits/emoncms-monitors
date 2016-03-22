9bb1d05fb9478c9d59db71775b603f1f00142171
James Swineson <jamesswineson@gmail.com>
2016-03-23 07:01:01 +0800

    Prevent being killed

--- os_report.py
+++ os_report.py
@@ -10,11 +10,21 @@ from subprocess import Popen, PIPE
 import time
 
 apikey = "2032057e1f79261b04bfd9a17ffc90eb"
-computer_name = "MacBook Pro"
+computer_name = "mbp"
 interval = 10
 
 def construct_name(n):
-    return computer_name + " - " + n
+    return computer_name + "-" + n
+
+def run(command):
+    p = Popen(command, stdout=PIPE)
+    return p.communicate()[0]
+
+def get_cpu_temp():
+    return float(run([os.path.join(os.path.dirname(os.path.realpath(__file__)), "osx-cpu-temp", "osx-cpu-temp")]).decode()[:-3])
+
+def get_battery_level():
+    return float(run(["pmset", "-g", "batt"]).decode().split("\n")[1].split()[1][:-2])
 
 def send(data):
     payload = {
@@ -27,20 +37,24 @@ def send(data):
 
 if __name__ == "__main__":
     while True:
-        a, b, c = os.getloadavg()
-        p = Popen([os.path.join(os.path.dirname(os.path.realpath(__file__)), "osx-cpu-temp", "osx-cpu-temp")], stdout=PIPE)
-        cpu_temp = float(p.communicate()[0].decode()[:-3])
-        data = {
-            construct_name(r"boot time"): psutil.boot_time(),
-            construct_name(r"load average - 1min"): a,
-            construct_name(r"load average - 5min"): b,
-            construct_name(r"load average - 15min"): c,
-            construct_name(r"CPU usage"): psutil.cpu_percent(),
-            construct_name(r"memory usage"): psutil.virtual_memory().percent / 100,
-            construct_name(r"system disk usage"): psutil.disk_usage('/').percent / 100,
-            construct_name(r"process count"): len(psutil.pids()),
-            construct_name(r"CPU temperature"): cpu_temp
-        }
-        print("Sending: ", data)
-        send(data)
-        time.sleep(interval)
+        try:
+            a, b, c = os.getloadavg()
+            data = {
+                construct_name(r"uptime"): psutil.boot_time(),
+                construct_name(r"load-1min"): a,
+                construct_name(r"load-5min"): b,
+                construct_name(r"load-15min"): c,
+                construct_name(r"cpu-percent"): psutil.cpu_percent(),
+                construct_name(r"memory-percent"): psutil.virtual_memory().percent,
+                construct_name(r"system-disk-percent"): psutil.disk_usage('/').percent,
+                construct_name(r"process-count"): len(psutil.pids()),
+                construct_name(r"cpu-temp"): get_cpu_temp(),
+                construct_name(r"battery"): get_battery_level()
+            }
+            print("Sending: ", data)
+            send(data)
+            time.sleep(interval)
+        except KeyboardInterrupt:
+            break
+        except:
+            pass
